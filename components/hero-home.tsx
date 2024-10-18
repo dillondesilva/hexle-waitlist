28e61713bb050987d64247e97557efb23b805609
dillondesilva <dillon.desilva@geeksdiary.com>
2024-10-18 11:39:51 +1100

    updated layout and text

--- components/hero-home.tsx
+++ components/hero-home.tsx
@@ -12,55 +12,7 @@ export default function HeroHome() {
         <div className="pb-12 pt-32 md:pb-20 md:pt-40">
           {/* Section header */}
           <div className="pb-12 text-center md:pb-16">
-            {/* <div
-              className="mb-6 border-y [border-image:linear-gradient(to_right,transparent,theme(colors.slate.300/.8),transparent)1]"
-              data-aos="zoom-y-out"
-            >
-              <div className="-mx-0.5 flex justify-center -space-x-3">
-                <Image
-                  className="box-content rounded-full border-2 border-gray-50"
-                  src={Avatar01}
-                  width={32}
-                  height={32}
-                  alt="Avatar 01"
-                />
-                <Image
-                  className="box-content rounded-full border-2 border-gray-50"
-                  src={Avatar02}
-                  width={32}
-                  height={32}
-                  alt="Avatar 01"
-                />
-                <Image
-                  className="box-content rounded-full border-2 border-gray-50"
-                  src={Avatar03}
-                  width={32}
-                  height={32}
-                  alt="Avatar 02"
-                />
-                <Image
-                  className="box-content rounded-full border-2 border-gray-50"
-                  src={Avatar04}
-                  width={32}
-                  height={32}
-                  alt="Avatar 03"
-                />
-                <Image
-                  className="box-content rounded-full border-2 border-gray-50"
-                  src={Avatar05}
-                  width={32}
-                  height={32}
-                  alt="Avatar 04"
-                />
-                <Image
-                  className="box-content rounded-full border-2 border-gray-50"
-                  src={Avatar06}
-                  width={32}
-                  height={32}
-                  alt="Avatar 05"
-                />
-              </div>
-            </div> */}
+           
             <h1
               className="mb-6 text-5xl font-bold md:text-6xl"
               data-aos="zoom-y-out"
@@ -81,7 +33,7 @@ export default function HeroHome() {
                 with the right AI tools to produce higher quality research
               </p>
               <Waitlist />
-              <div className="relative before:absolute before:inset-0 before:border-y before:[border-image:linear-gradient(to_right,transparent,theme(colors.slate.300/.8),transparent)1]">
+              <div className="relative before:absolute before:inset-0 before:border-y before:[border-image:linear-gradient(to_right,transparent,theme(colors.indigo.300/.8),transparent)1]">
                 <div
                   className="mx-auto max-w-xs sm:flex sm:max-w-none sm:justify-center"
                   data-aos="zoom-y-out"
@@ -115,7 +67,8 @@ export default function HeroHome() {
             data-aos-delay={600}
           >
             <div className="relative transition-opacity ease-in duration-1200 opacity-100 rounded-2xl px-5 py-3 before:pointer-events-none before:absolute before:-inset-5 before:border-y  after:absolute after:-inset-5 after:-z-10">
-              <img src="images/hexleuidesigns.png" className="rounded-2xl"></img>
+              {/* <img src="images/hexleuidesigns.png" className="rounded-2xl shadow-2xl"></img> */}
+              <video src="hexlevid1.mp4" className="rounded-2xl shadow-2xl" autoPlay muted={true} loop={true}/>
               {/* <div className="relative mb-8 flex items-center justify-between before:block before:h-[9px] before:w-[41px] before:bg-[length:16px_9px] before:[background-image:radial-gradient(circle_at_4.5px_4.5px,_theme(colors.gray.600)_4.5px,_transparent_0)] after:w-[41px]">
                 <span className="text-[13px] font-medium text-white">
                   cruip.com
--- components/large-testimonial.tsx
+++ components/large-testimonial.tsx
@@ -7,6 +7,8 @@ export default function LargeTestimonial() {
       <div className="mx-auto max-w-2xl px-4 sm:px-6">
         <div className="py-12 md:py-20">
           <div className="space-y-3 text-center transition-opacity ease-in duration-1200 opacity-100">
+            <h1 className="text-3xl font-bold">We are a dedicated, small team based in Sydney, Australia</h1>
+            <p>Frustrated with current research tools, we’re building better solutions to help researchers focus on their work, not the software.</p>
             <img src="images/visualisation.png"></img>
           </div>
         </div>
